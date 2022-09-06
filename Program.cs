c9e67d5b233ca894c4ffef1bf73b884e43baa709
abbosturaboev <112198647+abbosturaboev@users.noreply.github.com>
2022-09-06 19:35:11 +0300

    massive

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int[] number = new int[4];
+int[] num = {1, 2 , 5, 637, 124, 51, 25};
+Console.WriteLine(num[3] + 100 * 2);
+
+string[] array = {"af", "as", "2a", "afvz"};
+
+Console.WriteLine(String.Join(" ", array));
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
