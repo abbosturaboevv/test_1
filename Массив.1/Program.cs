2c5885d5eae993b2f556dec13a10c30efed481dd
abbosturaboev <112198647+abbosturaboev@users.noreply.github.com>
2022-09-06 21:03:15 +0300

    multiple

---
+++ Массив.1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Ввидете число ");
+int num_user = int.Parse(Console.ReadLine()); // Convert.ToInt32() = int.Parse()
+int[] number = new int[num_user];
+for (int i = 0; i < number.Length; i++)
+{
+    number[i] = new Random().Next(1, 10);
+}
+Console.WriteLine(String.Join(" ", number));
+
+MultipleNumberArray(number,4);
+MultipleNumberArray(number,5);
+MultipleNumberArray(number,6);
+MultipleNumberArray(number,7);
+
+void MultipleNumberArray(int[] array, int multiple)
+
+{
+    int count = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if(array[j] % multiple == 0)
+        {
+            Console.Write(array[j] + " ");
+            count++;
+        }
+    }
+    Console.WriteLine("Количество числе кратная " + multiple + " = " + count);
+}
