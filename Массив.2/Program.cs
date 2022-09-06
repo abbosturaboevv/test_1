860813f016e9312ab2cee8ba8abb99813f15b2fe
abbosturaboev <112198647+abbosturaboev@users.noreply.github.com>
2022-09-06 21:23:42 +0300

    number

---
+++ Массив.2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Ввидете число ");
+int num_user = int.Parse(Console.ReadLine()); // Convert.ToInt32() = int.Parse()
+int[] number = new int[num_user];
+for (int i = 0; i < number.Length; i++)
+{
+    number[i] = new Random().Next(-999,999); // -994 -141 -135
+}
+Console.WriteLine(String.Join(" ", number));
+
+int min = number[0];
+for (int i = 0; i < number.Length; i++)
+{
+    if(min > number[i])
+    {
+        min = number[i];
+    }
+    
+}
