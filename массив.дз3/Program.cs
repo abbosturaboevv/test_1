d3642579c64dccb54a4d32eb28edb2fa7cddfccc
abbosturaboev <112198647+abbosturaboev@users.noreply.github.com>
2022-09-08 19:10:55 +0300

    День недели

---
+++ массив.дз3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Ввидите цифру, обозначающую день недели");
+int dayOfWeek = int.Parse(Console.ReadLine());
+if (dayOfWeek<8 && dayOfWeek>0)
+{
+    if(dayOfWeek==6 || dayOfWeek==7)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Будный");
+    }
+}
+else
+{
+   Console.WriteLine("Число выходит за диапазон [1;7]");
+}
