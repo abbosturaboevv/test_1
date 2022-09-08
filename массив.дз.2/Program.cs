dfd450e1dc557e53af50ed3634cdce6b434e75b4
abbosturaboev <112198647+abbosturaboev@users.noreply.github.com>
2022-09-08 18:54:35 +0300

    третья цифра

---
+++ массив.дз.2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+int Number = new Random().Next(0,9999);
+Console.WriteLine(Number);
+string dumbNaming = Convert.ToString(Number);
+if (dumbNaming.Length>2)
+{
+   Console.WriteLine(dumbNaming[2] + " - наша третья цифра");
+}
+else
+{
+   Console.WriteLine("третьей цифры нет");
+}
